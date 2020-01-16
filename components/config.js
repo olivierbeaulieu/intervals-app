141593a9a0241bb6e0306dc3c20b6e6e535461cd
Olivier Beaulieu <olivierbeaulieu657@gmail.com>
2020-01-16 18:48:36 -0500

    Added many more scales

--- components/config.js
+++ components/config.js
@@ -23,4 +23,53 @@ export const SCALES_MODES = {
     displayName: 'Natural Minor',
     pattern: [2, 1, 2, 2, 1, 2, 2],
   },
+  majorPentatonic: {
+    displayName: 'Major Pentatonic',
+    pattern: [2, 2, 3, 2, 3],
+  },
+  minorPentatonic: {
+    displayName: 'Minor Pentatonic',
+    pattern: [3, 2, 2, 3, 2],
+  },
+  blues: {
+    displayName: 'Blues Pentatonic',
+    pattern: [3, 2, 1, 1, 3, 2],
+  },
+  dorian: {
+    displayName: 'Dorian',
+    pattern: [2, 1, 2, 2, 2, 1, 2],
+  },
+  phrygian: {
+    displayName: 'Phrygian',
+    pattern: [1, 2, 2, 2, 1, 2, 2],
+  },
+  phrygianDominant: {
+    displayName: 'Phrygian Dominant',
+    pattern: [1, 3, 1, 2, 1, 2, 2],
+  },
+  harmonicMajor: {
+    displayName: 'Harmonic Major',
+    pattern: [2, 2, 1, 2, 1, 3, 1],
+  },
+  harmonicMinor: {
+    displayName: 'Harmonic Minor',
+    pattern: [2, 1, 2, 2, 1, 3, 1],
+  },
+  hungarianMinor: {
+    displayName: 'Hungarian Minor',
+    pattern: [2, 1, 3, 1, 1, 3, 1],
+  },
+
+  locrian: {
+    displayName: 'Locrian',
+    pattern: [1, 2, 2, 1, 2, 2, 2],
+  },
+  lydian: {
+    displayName: 'Lydian',
+    pattern: [1, 1, 1, 2, 1, 1, 2],
+  },
+  mixolydian: {
+    displayName: 'Mixolydian',
+    pattern: [2, 2, 1, 2, 2, 1, 2],
+  },
 };
